86c6627e4e3e1ebe28c141bbe93facccf8d6c48a
witrikomalasari <dede.witri@gmail.com>
2024-06-14 03:36:47 +0700

    fix: Theme folder

--- dot_indonesia/src/screens/Home/Home.tsx
+++ dot_indonesia/src/screens/Home/Home.tsx
@@ -3,7 +3,7 @@ import {HeaderApp} from '@components/Molecules';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Beranda from '@screens/Beranda/Beranda';
 import FavoriteScreen from '@screens/Favorite/FavoriteScreen';
-import {DOTColors} from '@theme/DotColors';
+import {DOTColors} from '@screens/theme/DotColors';
 import React, {FC} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
